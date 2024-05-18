3fe23dc54cc43da499f834ac9a14820c3db23fb1
ChamikaRohan <chamika707@gmail.com>
2024-05-18 21:02:07 +0530

    Created basic backend structure

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express"
+import mongoose from "mongoose"
+import bodyParser from "body-parser"
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+const DB_URL = process.env.MONGODB ;
+
+mongoose.connect(DB_URL).then(()=>{
+    console.log("Database connected successfully!")
+}).catch((err)=>{
+    console.log("Database connection failed with error: ",err.message)
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on PORT: ${PORT}`)
+})
